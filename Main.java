f654b2ddd1a50d6ce5ed651e35c640c5221bee02
Lee, Seulah <sarhee1004@naver.com>
2024-11-20 09:47:52 +0900

    메시지 four

--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ System.out.println("202214068 이슬아");
 System.out.println(Hello.sum(10));
 System.out.println(Hello.sum(100));
 System.out.println(Hello.sum(1000));
+System.out.println(World.factorial(3));
+System.out.println(World.factorial(6));
+System.out.println(World.factorial(9));
 }
 }
