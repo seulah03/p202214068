b3fc04950525469ab24fc2aa9873003ce97b7539
Lee, Seulah <sarhee1004@naver.com>
2024-11-20 09:45:01 +0900

    메시지 three

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public static void main(String[] args) {
 System.out.println("202214068 이슬아");
 System.out.println(Hello.sum(10));
 System.out.println(Hello.sum(100));
+System.out.println(Hello.sum(1000));
 }
 }
